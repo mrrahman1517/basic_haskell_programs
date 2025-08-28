6eb8e570af444ade57198db0e4649993e59e906f
Muntasir Raihan Rahman <mrahman@keystonestrategy.com>
2025-08-27 19:48:09 -0700

    add simple recursive funcs

--- recursion.hs
+++ recursion.hs
@@ -4,3 +4,19 @@ fib :: Int -> Int
 fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
+
+-- factorial
+fac :: Int -> Int 
+fac n = product [1..n]
+
+facr :: Int -> Int 
+facr 0 = 1
+facr n = n * facr (n-1)
+
+myproduct :: Num a => [a] -> a
+myproduct [] = 1
+myproduct (n:ns) = n * myproduct ns
+
+mylength :: [a] -> Int
+mylength [] = 0
+mylength (_:xs) = 1 + mylength xs
