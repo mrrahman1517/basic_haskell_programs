6a1d191e8fefe19a8ef9c8913841524d5ca83d8b
Muntasir Raihan Rahman <mrahman@keystonestrategy.com>
2025-09-04 20:49:53 -0700

    start higher order function examples

---
+++ hof.hs
@@ -0,0 +1,15 @@
+twice :: (a->a)->a->a
+twice f x = f (f x)
+
+sqr :: Int -> Int 
+sqr x = x * x
+
+plusv3 :: Int -> Int -> Int 
+plusv3 x y = x + y
+
+incv3 :: Int -> Int 
+incv3 = plusv3 1
+
+-- twice incv3 31
+-- map (plusv3 4) [1,2,3]
+-- 
