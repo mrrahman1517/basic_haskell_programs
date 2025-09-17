94f28531d5b05c65b06f2560ca510d2e9089901c
Muntasir Raihan Rahman <mrahman@keystonestrategy.com>
2025-09-16 21:26:39 -0700

    add

--- MyIO.hs
+++ MyIO.hs
@@ -1,49 +1,49 @@
-
 module MyIO (
-        act,
-        mygetLine,
-        myputStr,
-        myputStrLn,
-        mystrlen,
-        showstrlen
+  act,
+  mygetLine,
+  myputStr,
+  myputStrLn,
+  mystrlen,
+  showstrlen
 ) where
 
 -- interactive programming
 
 act :: IO (Char, Char, Char)
-act = do x <- getChar 
-                 y <- getChar
-                 z <- getChar 
-                 return (x, y, z)
+act = do
+  x <- getChar
+  y <- getChar
+  z <- getChar
+  return (x, y, z)
 
-mygetLine :: IO String 
-mygetLine = do x <- getChar 
-                             if x == '\n' then 
-                                 return []
-                             else 
-                                 do xs <- mygetLine 
-                                        return (x:xs)
+mygetLine :: IO String
+mygetLine = do
+  x <- getChar
+  if x == '\n'
+    then return []
+    else do
+      xs <- mygetLine
+      return (x : xs)
 
 myputStr :: String -> IO ()
 myputStr [] = return ()
-myputStr (x:xs) = do putChar x
-                                         myputStr xs
+myputStr (x:xs) = do
+  putChar x
+  myputStr xs
 
-myputStrLn :: String -> IO () 
-myputStrLn xs = do myputStr xs
-                                     putChar '\n'
+myputStrLn :: String -> IO ()
+myputStrLn xs = do
+  myputStr xs
+  putChar '\n'
 
-mystrlen :: String -> Int 
-mystrlen [] = 0
-mystrlen (x:xs) = 1 + mystrlen xs
+mystrlen :: String -> Int
+mystrlen []     = 0
+mystrlen (_:xs) = 1 + mystrlen xs
 
 showstrlen :: IO ()
-showstrlen = do myputStr "Enter a string: "
-                                xs <- mygetLine
-                                myputStr "The string has "
-                                myputStr (show (mystrlen xs))
-                                myputStrLn " chars"
-
--- Add your function definitions below
--- exampleFunction :: String -> IO ()
--- exampleFunction s = putStrLn s
+showstrlen = do
+  myputStr "Enter a string: "
+  xs <- mygetLine
+  myputStr "The string has "
+  myputStr (show (mystrlen xs))
+  myputStrLn " chars"
--- hangman.hs
+++ hangman.hs
@@ -1,7 +1,38 @@
 import MyIO
+import System.IO
 
 hangman :: IO ()
 hangman = do myputStrLn "Think of a word: "
              word <- mysgetLine
              myputStrLn "Try to guess it:"
              play word 
+
+mysgetLine :: IO String 
+mysgetLine = do x <- mygetCh 
+                if x == '\n' then 
+                    do putChar x
+                       return []
+                else 
+                    do putChar '-'
+                       xs <- mysgetLine 
+                       return (x:xs)
+
+mygetCh :: IO Char 
+mygetCh = do hSetEcho stdin False
+             x <- getChar 
+             hSetEcho stdin True 
+             return x
+
+play :: String -> IO ()
+play word = 
+    do myputStr "? "
+       guess <- mygetLine
+       if guess == word then 
+          myputStrLn "You got it!"
+       else 
+          do myputStrLn (match word guess)
+             play word 
+
+match :: String -> String -> String 
+match xs ys = 
+    [if elem x ys then x else '-'| x <- xs]
