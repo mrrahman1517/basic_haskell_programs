af29542e04a5a7ad134fecfe57b454af9d950bc2
Muntasir Raihan Rahman <mrahman@keystonestrategy.com>
2025-09-19 08:45:44 -0700

    nim

--- nim.hs
+++ nim.hs
@@ -20,3 +20,19 @@ finished :: Board -> Bool
 --finished [x] = x == 0
 --finished (b:bs) = b == 0 && finished bs
 finished b = all (==0) b
+
+valid :: Board -> Int -> Int -> Bool 
+valid b row num = b !! (row-1) >= num
+
+-- assume move is valid using previous check
+--move :: Board -> Int -> Int -> Board
+--move b row num = [adjust r n | (r,n) <- zip [1..5] b]
+--                 where
+--                    adjust r n = if r == row then n-num else n 
+
+-- assume move is valid using previous check
+move :: Board -> Int -> Int -> Board
+move b row num =
+  map (\(r, n) -> if r == row then n - num else n) (zip [1..5] b)
+
+
